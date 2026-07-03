ff985e1f327cc731123bae393faecde7047d3634
farmei <farmei@proton.me>
2026-07-03 18:25:35 +0000

    rtwo: Do not package firmware images

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,24 +1,3 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
-
-AB_OTA_PARTITIONS += \
-    abl \
-    aop \
-    bluetooth \
-    cpucp \
-    devcfg \
-    dsp \
-    fsg \
-    hyp \
-    keymaster \
-    logo \
-    modem \
-    prov \
-    qupfw \
-    shrm \
-    storsec \
-    tz \
-    uefisecapp \
-    xbl \
-    xbl_config
